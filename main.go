1c49eae00c2639439823e5374f0e29d7f474d7b8
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2024-07-20 13:05:44 -0400

    clean html and main.go

--- main.go
+++ main.go
@@ -149,6 +149,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", handler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	fmt.Println("Starting server at :8080")
+	fmt.Println("Starting server at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
